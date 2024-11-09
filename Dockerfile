1fda9137bc6902c2ccea0b7726c9fc61cc190376
Sagar Shah <sagardnshah@gmail.com>
2024-11-09 17:29:14 -0500

    more boilerplate iteration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM python:3.9
+
 ARG NAME
+ARG CLINET_CONFIG_DIR
+
+RUN mkdir -p $CLINET_CONFIG_DIR
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir google-auth
 
 WORKDIR /$NAME
 CMD [ "python", "backupgp.py" ]
--- setup.sh
+++ setup.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-source ./env
+NAME="backupgp"
+CLINET_CONFIG_DIR="/client_config"
+CLIENT_ID="$HOME/config/google-client-credentials.json"
 
 # 'sudo -v' prompts for password once and caches the credentials (default:15mins).
 # If sudo in front of a command, then run as root. Else, run as current user.
 sudo -v || { echo "This script requires sudo access. Exiting."; exit 1; }
 
+function clean () {
+  sudo docker container prune -f
+}
+
 function build () {
-  sudo docker build --build-arg NAME="$NAME" -t $NAME .
+  sudo docker build \
+  --build-arg NAME="$NAME" \
+  --build-arg CLINET_CONFIG_DIR="$CLINET_CONFIG_DIR" \
+  -t $NAME .
 }
 
 function debug () {
-  sudo docker run --env-file env -v $(pwd):/$NAME -it $NAME /bin/bash
+  sudo docker run \
+  -v $(pwd):/$NAME \
+  -v $CLIENT_ID:/$CLINET_CONFIG_DIR/$(basename $CLIENT_ID) \
+  -it $NAME /bin/bash
 }
 
 function run () {
-  sudo docker run --env-file env -v $(pwd):/$NAME $NAME
+  sudo docker run \
+  -v $(pwd):/$NAME \
+  -v $CLIENT_ID:/$CLINET_CONFIG_DIR/$(basename $CLIENT_ID) \
+  $NAME
 }
 
+clean
 build
 debug
 #run
